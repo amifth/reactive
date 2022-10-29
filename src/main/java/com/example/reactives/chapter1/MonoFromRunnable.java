b0fb9c761cc38b89db1349a0eb72a2635938a344
amifth <miftakhul.14116078@student.itera.ac.id>
2022-10-29 20:13:13 +0700

    add mono from runnable

--- src/main/java/com/example/reactives/chapter1/MonoFromRunnable.java
+++ src/main/java/com/example/reactives/chapter1/MonoFromRunnable.java
@@ -1,8 +1,20 @@
 package com.example.reactives.chapter1;
 
 import com.example.reactives.utils.Utils;
+import reactor.core.publisher.Mono;
 
 public class MonoFromRunnable {
+
+    public static void main(String[] args) {
+        Mono.fromRunnable(timeConsumingProcess())
+                .subscribe(Utils.onNext(),
+                        Utils.onError(),
+                        () -> {
+                            System.out.println("process is done. Sending emails...");
+                        });
+
+
+    }
 
     private static Runnable timeConsumingProcess(){
         return () -> {
