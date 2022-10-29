7716146faa978acf59dc1df27cd67178af751867
amifth <miftakhul.14116078@student.itera.ac.id>
2022-10-29 20:10:28 +0700

    add mono from future

---
+++ src/main/java/com/example/reactives/chapter1/MonoFromFuture.java
@@ -0,0 +1,20 @@
+package com.example.reactives.chapter1;
+
+import com.example.reactives.utils.Utils;
+import reactor.core.publisher.Mono;
+
+import java.util.concurrent.CompletableFuture;
+
+public class MonoFromFuture {
+
+    public static void main(String[] args) {
+        Mono.fromFuture(getName())
+                .subscribe(Utils.onNext());
+
+        Utils.sleepSeconds(1);
+    }
+
+    private static CompletableFuture<String> getName(){
+        return CompletableFuture.supplyAsync(() -> Utils.faker().name().fullName());
+    }
+}
